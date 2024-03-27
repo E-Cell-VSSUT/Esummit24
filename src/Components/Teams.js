d78a9c4c7e02fba6f57a8c84fe73eb1676aa429a
aharekrushna <aharekrushnanayak@gmail.com>
2024-03-27 13:56:57 +0530

    Update to constants/index.js: added new properties for navLinks, teams, events, pastGuests, pastSpon, mediaTeam, techTeam, speakerteam, eventTeam, sponsorteam, designteam, Agenda, and speakers
    In this update, the constants/index.js file has been modified to include new properties and data. The navLinks array has been updated with new objects, each containing id, title, color, and route properties. The teams object has been updated with a new lead array, which contains objects with name, designation, image URL, and social media links. The events array has been updated with new objects, each containing id, title, logo URL, and content properties. The pastGuests and pastSpon objects have been added with new lead arrays, which contain objects with name, designation, image URL, and social media links. The mediaTeam, techTeam, speakerteam, eventTeam, sponsorteam, and designteam objects have been added with new lead arrays, which contain objects with name, designation, image URL, and social media links. The Agenda object has been added with Day1 and Day2 arrays, which contain objects with venue, topic, time, description, speaker, image URL, and designation properties. The speakers object has been added with a lead array, which contains objects with name, designation, image URL, and social media links.

--- src/Components/Teams.js
+++ src/Components/Teams.js
@@ -9,9 +9,12 @@ import { CiInstagram } from "react-icons/ci";
 const Teams = () => {
   return (
     <div className="flex flex-col justify-center items-center h-full w-full gap-10">
-      <div className="flex flex-col justify-center items-center gap-20 sm:gap-5 w-full mt-[150px] ">
+      <h1 className="text-2xl xl:text-6xl text-white mt-[9rem]">
+          The Team
+        </h1>
+      <div className="flex flex-col justify-center items-center gap-20 sm:gap-5 w-full mt-[15px] ">
         <h1 className="text-2xl xl:text-5xl text-white">
-          For any Media related Queries
+        For any Website and Technical Queries
         </h1>
         <div className="team_container">
           {teams.lead.map((team) => (
@@ -64,9 +67,9 @@ const Teams = () => {
         </div>
       </div>
 
-      <div className="flex flex-col justify-center items-center gap-20 sm:gap-5 w-full">
-      <h1 className="text-2xl xl:text-5xl text-white">
-          For any Media related Queries
+      <div className="flex flex-col justify-center items-center gap-20 sm:gap-5 w-full mt-[15px] ">
+        <h1 className="text-2xl xl:text-5xl text-white">
+        For any Speaker related Queries
         </h1>
         <div className="team_container">
           {teams.lead.map((team) => (
@@ -119,9 +122,9 @@ const Teams = () => {
         </div>
       </div>
 
-      <div className="flex flex-col justify-center items-center gap-20 sm:gap-5 w-full">
-      <h1 className="text-2xl xl:text-5xl text-white">
-          For any Media related Queries
+      <div className="flex flex-col justify-center items-center gap-20 sm:gap-5 w-full mt-[15px] ">
+        <h1 className="text-2xl xl:text-5xl text-white">
+        For any Events realted Queries
         </h1>
         <div className="team_container">
           {teams.lead.map((team) => (
@@ -174,10 +177,9 @@ const Teams = () => {
         </div>
       </div>
 
-
-      <div className="flex flex-col justify-center items-center gap-20 sm:gap-5 w-full">
-         <h1 className="text-2xl xl:text-5xl text-white">
-          For any Media related Queries
+      <div className="flex flex-col justify-center items-center gap-20 sm:gap-5 w-full mt-[15px] ">
+        <h1 className="text-2xl xl:text-5xl text-white">
+        For any Media related Queries
         </h1>
         <div className="team_container">
           {teams.lead.map((team) => (
@@ -230,9 +232,119 @@ const Teams = () => {
         </div>
       </div>
 
-      <div className="flex flex-col justify-center items-center gap-20 sm:gap-5 w-full">
-      <h1 className="text-2xl xl:text-5xl text-white">
-          For any Media related Queries
+      <div className="flex flex-col justify-center items-center gap-20 sm:gap-5 w-full mt-[15px] ">
+        <h1 className="text-2xl xl:text-5xl text-white">
+        For any Sponsorship related Queries
+        </h1>
+        <div className="team_container">
+          {teams.lead.map((team) => (
+            <div class="wrapper ">
+              <div class="img-area">
+                <div class="inner-area">
+                  <img src={team.url} alt="" />
+                </div>
+              </div>
+              <div class="effect"></div>
+              <div class="name">{team.name}</div>
+              <div class="secname">{team.name}</div>
+              <div class="about">{team.designation}</div>
+              <div class="social-icons">
+                <a
+                  href="#"
+                  className=" XTwitter flex justify-center items-center text-2xl"
+                >
+                  <div className="z-40 flex justify-center items-center text-zinc-100 iconbeat ">
+                    <FaXTwitter />
+                  </div>
+                </a>
+                <a
+                  href="#"
+                  className="Facebook flex justify-center items-center text-2xl"
+                >
+                  <div className="z-40 flex justify-center items-center text-zinc-100 iconbeat ">
+                    <FaFacebook />
+                  </div>
+                </a>
+                <a
+                  href="#"
+                  className="Linkedin flex justify-center items-center text-2xl"
+                >
+                  <div className="z-40 flex justify-center items-center text-zinc-100 iconbeat ">
+                    <LiaLinkedinIn />
+                  </div>
+                </a>
+                <a
+                  href="#"
+                  className="Instagram flex justify-center items-center text-2xl"
+                >
+                  <div className="z-40 flex justify-center items-center text-zinc-100 iconbeat ">
+                    <CiInstagram />
+                  </div>
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex flex-col justify-center items-center gap-20 sm:gap-5 w-full mt-[15px] ">
+        <h1 className="text-2xl xl:text-5xl text-white">
+        For any Design related Queries
+        </h1>
+        <div className="team_container">
+          {teams.lead.map((team) => (
+            <div class="wrapper ">
+              <div class="img-area">
+                <div class="inner-area">
+                  <img src={team.url} alt="" />
+                </div>
+              </div>
+              <div class="effect"></div>
+              <div class="name">{team.name}</div>
+              <div class="secname">{team.name}</div>
+              <div class="about">{team.designation}</div>
+              <div class="social-icons">
+                <a
+                  href="#"
+                  className=" XTwitter flex justify-center items-center text-2xl"
+                >
+                  <div className="z-40 flex justify-center items-center text-zinc-100 iconbeat ">
+                    <FaXTwitter />
+                  </div>
+                </a>
+                <a
+                  href="#"
+                  className="Facebook flex justify-center items-center text-2xl"
+                >
+                  <div className="z-40 flex justify-center items-center text-zinc-100 iconbeat ">
+                    <FaFacebook />
+                  </div>
+                </a>
+                <a
+                  href="#"
+                  className="Linkedin flex justify-center items-center text-2xl"
+                >
+                  <div className="z-40 flex justify-center items-center text-zinc-100 iconbeat ">
+                    <LiaLinkedinIn />
+                  </div>
+                </a>
+                <a
+                  href="#"
+                  className="Instagram flex justify-center items-center text-2xl"
+                >
+                  <div className="z-40 flex justify-center items-center text-zinc-100 iconbeat ">
+                    <CiInstagram />
+                  </div>
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex flex-col justify-center items-center gap-20 sm:gap-5 w-full mt-[15px] ">
+        <h1 className="text-2xl xl:text-5xl text-white">
+        Overall Coordinators
         </h1>
         <div className="team_container">
           {teams.lead.map((team) => (
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,105 +1,399 @@
-import {
-    back,logo,bg
-} from "../assets"
-
+import { back, logo, bg } from "../assets";
 
 export const navLinks = [
-    {
-      id: "home",
-      title: "Home",
-      color:'#',
-    },
-    {
-      id: "event",
-      title: "Events",
-      color:'#',
-    },
-    {
-      id: "Sponsors",
-      title: "Sponsors",
-      color:'#',
-    },
-    {
-      id: "speakers",
-      title: "Speakers",
-      color:'#',
-    },
+  {
+    id: "home",
+    title: "Home",
+    color: "#",
+  },
+  {
+    id: "event",
+    title: "Events",
+    color: "#",
+  },
+  {
+    id: "Sponsors",
+    title: "Sponsors",
+    color: "#",
+  },
+  {
+    id: "speakers",
+    title: "Speakers",
+    color: "#",
+  },
 
+  {
+    id: "contact",
+    title: "Contact",
+    color: "#",
+  },
+  {
+    id: "agenda",
+    title: "Agenda",
+    color: "#00df9a",
+  },
+];
+
+export const teams = {
+  lead: [
     {
-      id: "contact",
-      title: "Contact",
-      color:'#',
+      name: "A ",
+      designation: "tech lead",
+      url: "https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      ilink: "#",
+      flink: "#",
+      llink: "https://www.linkedin.com/in/anshu-agrawal-b517b9244/",
+      tlink: "https://twitter.com/008Anshu",
     },
     {
-      id: "agenda",
-      title: "Agenda",
-      color:'#00df9a',
+      name: "Y ",
+      designation: "tech lead ",
+      url: "https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      ilink: "https://instagram.com/__shradha_032?igshid=Y2M0YTlkZGNmOQ==",
+      flink: "#",
+      llink: "https://www.linkedin.com/in/shradha-mohapatra-818b05238",
+      tlink: "#",
     },
-  ];
+  ],
+};
 
-  export const teams = {
-    lead: [
-      {
-        name: "A HareKrishna nayak",
-        designation: "Co-ordinator",
-        url:"https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-        ilink: "#",
-        flink: "#",
-        llink: "https://www.linkedin.com/in/anshu-agrawal-b517b9244/",
-        tlink: "https://twitter.com/008Anshu",
-      },
-      {
-        name: "Yashika Sharma ",
-        designation: "Co-ordinator ",
-        url:"https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-        ilink: "https://instagram.com/__shradha_032?igshid=Y2M0YTlkZGNmOQ==",
-        flink: "#",
-        llink: "https://www.linkedin.com/in/shradha-mohapatra-818b05238",
-        tlink: "#",
-      }
-    ]}
+export const events = [
+  {
+    id: 1,
+    title: "Content Creation Conclave",
+    logo: "https://2k21.s3.amazonaws.com/event_logos/CCC.png",
+    content:
+      "Lorem ipsum dolor sit amet consectetadipisicing elit. Quidem idad saepe similique cupiditate dolor amet nemo dolore .",
+    regUrl: "",
+    eventDet: "",
+  },
+  {
+    id: 1,
+    title: "Content Creation Conclave",
+    logo: "https://2k21.s3.amazonaws.com/event_logos/CCC.png",
+    content:
+      "Lorem ipsum dolor sit amet consectetadipisicing elit. Quidem idad saepe similique cupiditate dolor amet nemo dolore .",
+    regUrl: "",
+    eventDet: "",
+  },
+  {
+    id: 1,
+    title: "Content Creation Conclave",
+    logo: "https://2k21.s3.amazonaws.com/event_logos/CCC.png",
+    content:
+      "Lorem ipsum dolor sit amet consectetadipisicing elit. Quidem idad saepe similique cupiditate dolor amet nemo dolore .",
+    regUrl: "",
+    eventDet: "",
+  },
+  {
+    id: 1,
+    title: "Content Creation Conclave",
+    logo: "https://2k21.s3.amazonaws.com/event_logos/CCC.png",
+    content:
+      "Lorem ipsum dolor sit amet consectetadipisicing elit. Quidem idad saepe similique cupiditate dolor amet nemo dolore .",
+    regUrl: "",
+    eventDet: "",
+  },
+  {
+    id: 1,
+    title: "Content Creation Conclave",
+    logo: "https://2k21.s3.amazonaws.com/event_logos/CCC.png",
+    content:
+      "Lorem ipsum dolor sit amet consectetadipisicing elit. Quidem idad saepe similique cupiditate dolor amet nemo dolore .",
+    regUrl: "",
+    eventDet: "",
+  },
+  {
+    id: 1,
+    title: "Content Creation Conclave",
+    logo: "https://2k21.s3.amazonaws.com/event_logos/CCC.png",
+    content:
+      "Lorem ipsum dolor sit amet consectetadipisicing elit. Quidem idad saepe similique cupiditate dolor amet nemo dolore .",
+    regUrl: "",
+    eventDet: "",
+  },
+];
 
-    export const events=[
-      {
-        id:1,
-        title:"Content Creation Conclave",
-        logo:'https://2k21.s3.amazonaws.com/event_logos/CCC.png',
-        content:"Lorem ipsum dolor sit amet consectetadipisicing elit. Quidem idad saepe similique cupiditate dolor amet nemo dolore ."
-      },
-      {
-        id:1,
-        title:"Content Creation Conclave",
-        logo:'https://2k21.s3.amazonaws.com/event_logos/CCC.png',
-        content:"Lorem ipsum dolor sit amet consectetadipisicing elit. Quidem idad saepe similique cupiditate dolor amet nemo dolore ."
-      },
-      {
-        id:1,
-        title:"Content Creation Conclave",
-        logo:'https://2k21.s3.amazonaws.com/event_logos/CCC.png',
-        content:"Lorem ipsum dolor sit amet consectetadipisicing elit. Quidem idad saepe similique cupiditate dolor amet nemo dolore ."
-      },
-      {
-        id:1,
-        title:"Content Creation Conclave",
-        logo:'https://2k21.s3.amazonaws.com/event_logos/CCC.png',
-        content:"Lorem ipsum dolor sit amet consectetadipisicing elit. Quidem idad saepe similique cupiditate dolor amet nemo dolore ."
-      },
-      {
-        id:1,
-        title:"Content Creation Conclave",
-        logo:'https://2k21.s3.amazonaws.com/event_logos/CCC.png',
-        content:"Lorem ipsum dolor sit amet consectetadipisicing elit. Quidem idad saepe similique cupiditate dolor amet nemo dolore ."
-      },
-      {
-        id:1,
-        title:"Content Creation Conclave",
-        logo:'https://2k21.s3.amazonaws.com/event_logos/CCC.png',
-        content:"Lorem ipsum dolor sit amet consectetadipisicing elit. Quidem idad saepe similique cupiditate dolor amet nemo dolore ."
-      },
-      {
-        id:1,
-        title:"Content Creation Conclave",
-        logo:'https://2k21.s3.amazonaws.com/event_logos/CCC.png',
-        content:"Lorem ipsum dolor sit amet consectetadipisicing elit. Quidem idad saepe similique cupiditate dolor amet nemo dolore ."
-      }
-    ]
+export const pastGuests = {
+  lead: [
+    {
+      name: "A",
+      designation: "b",
+      imgUrl: "c",
+    },
+    {
+      name: "A",
+      designation: "b",
+      imgUrl: "c",
+    },
+  ],
+};
+
+export const pastSpon = {
+  lead: [
+    {
+      logo: "A",
+      brand: "B",
+    },
+    {
+      logo: "A",
+      brand: "B",
+    },
+    {
+      logo: "A",
+      brand: "B",
+    },
+  ],
+};
+
+export const mediaTeam = {
+  lead: [
+    {
+      name: "A ",
+      designation: "B",
+      url: "https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      ilink: "#",
+      flink: "#",
+      llink: "https://www.linkedin.com/in/anshu-agrawal-b517b9244/",
+      tlink: "https://twitter.com/008Anshu",
+    },
+    {
+      name: "A ",
+      designation: "B",
+      url: "https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      ilink: "#",
+      flink: "#",
+      llink: "https://www.linkedin.com/in/anshu-agrawal-b517b9244/",
+      tlink: "https://twitter.com/008Anshu",
+    },
+  ],
+};
+
+export const techTeam = {
+  lead: [
+    {
+      name: "A ",
+      designation: "B",
+      url: "https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      ilink: "#",
+      flink: "#",
+      llink: "https://www.linkedin.com/in/anshu-agrawal-b517b9244/",
+      tlink: "https://twitter.com/008Anshu",
+    },
+    {
+      name: "A ",
+      designation: "B",
+      url: "https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      ilink: "#",
+      flink: "#",
+      llink: "https://www.linkedin.com/in/anshu-agrawal-b517b9244/",
+      tlink: "https://twitter.com/008Anshu",
+    },
+  ],
+};
+
+export const speakerteam = {
+  lead: [
+    {
+      name: "A ",
+      designation: "B",
+      url: "https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      ilink: "#",
+      flink: "#",
+      llink: "https://www.linkedin.com/in/anshu-agrawal-b517b9244/",
+      tlink: "https://twitter.com/008Anshu",
+    },
+    {
+      name: "A ",
+      designation: "B",
+      url: "https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      ilink: "#",
+      flink: "#",
+      llink: "https://www.linkedin.com/in/anshu-agrawal-b517b9244/",
+      tlink: "https://twitter.com/008Anshu",
+    },
+  ],
+};
+
+export const eventTeam = {
+  lead: [
+    {
+      name: "A ",
+      designation: "B",
+      url: "https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      ilink: "#",
+      flink: "#",
+      llink: "https://www.linkedin.com/in/anshu-agrawal-b517b9244/",
+      tlink: "https://twitter.com/008Anshu",
+    },
+    {
+      name: "A ",
+      designation: "B",
+      url: "https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      ilink: "#",
+      flink: "#",
+      llink: "https://www.linkedin.com/in/anshu-agrawal-b517b9244/",
+      tlink: "https://twitter.com/008Anshu",
+    },
+  ],
+};
+
+export const sponsorteam = {
+  lead: [
+    {
+      name: "A ",
+      designation: "B",
+      url: "https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      ilink: "#",
+      flink: "#",
+      llink: "https://www.linkedin.com/in/anshu-agrawal-b517b9244/",
+      tlink: "https://twitter.com/008Anshu",
+    },
+    {
+      name: "A ",
+      designation: "B",
+      url: "https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      ilink: "#",
+      flink: "#",
+      llink: "https://www.linkedin.com/in/anshu-agrawal-b517b9244/",
+      tlink: "https://twitter.com/008Anshu",
+    },
+  ],
+};
+
+export const designteam = {
+  lead: [
+    {
+      name: "A ",
+      designation: "B",
+      url: "https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      ilink: "#",
+      flink: "#",
+      llink: "https://www.linkedin.com/in/anshu-agrawal-b517b9244/",
+      tlink: "https://twitter.com/008Anshu",
+    },
+    {
+      name: "A ",
+      designation: "B",
+      url: "https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      ilink: "#",
+      flink: "#",
+      llink: "https://www.linkedin.com/in/anshu-agrawal-b517b9244/",
+      tlink: "https://twitter.com/008Anshu",
+    },
+  ],
+};
+
+export const Agenda = {
+  Day1: [
+    {
+      venue: "A ",
+      topic: "tech lead",
+      time: "tech lead",
+      description: "tech lead",
+      speaker: "tech lead",
+      imgurl: "tech lead",
+      designation: "tech lead",
+    },
+    {
+      venue: "A ",
+      topic: "tech lead",
+      time: "tech lead",
+      description: "tech lead",
+      speaker: "tech lead",
+      imgurl: "tech lead",
+      designation: "tech lead",
+    },
+    {
+      venue: "A ",
+      topic: "tech lead",
+      time: "tech lead",
+      description: "tech lead",
+      speaker: "tech lead",
+      imgurl: "tech lead",
+      designation: "tech lead",
+    },
+  ],
+  Day2: [
+    {
+      venue: "A ",
+      topic: "tech lead",
+      time: "tech lead",
+      description: "tech lead",
+      speaker: "tech lead",
+      imgurl: "tech lead",
+      designation: "tech lead",
+    },
+    {
+      venue: "A ",
+      topic: "tech lead",
+      time: "tech lead",
+      description: "tech lead",
+      speaker: "tech lead",
+      imgurl: "tech lead",
+      designation: "tech lead",
+    },
+    {
+      venue: "A ",
+      topic: "tech lead",
+      time: "tech lead",
+      description: "tech lead",
+      speaker: "tech lead",
+      imgurl: "tech lead",
+      designation: "tech lead",
+    },
+  ],
+};
+
+export const speakers = {
+  lead: [
+    {
+      name: "A ",
+      designation: "B",
+      url: "https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      
+    },
+    {
+      name: "A ",
+      designation: "B",
+      url: "https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      
+    },
+    {
+      name: "A ",
+      designation: "B",
+      url: "https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      
+    },
+  ],
+};
+
+export const sponsors = {
+  lead: [
+    {
+      brand: "A ",
+      designation: "B",
+      logourl: "https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      
+    },
+    {
+      brand: "A ",
+      designation: "B",
+      logourl: "https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      
+    },
+    {
+      brand: "A ",
+      designation: "B",
+      logourl: "https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      
+    },
+    {
+      brand: "A ",
+      designation: "B",
+      logourl: "https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      
+    },
+  ],
+};
+
