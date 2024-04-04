83029a2623ea07fe55249ff539d6e99b243f5727
aharekrushna <aharekrushnanayak@gmail.com>
2024-04-04 12:02:14 +0530

    speaker

--- src/Components/Speaker.js
+++ src/Components/Speaker.js
@@ -7,38 +7,42 @@ import { CiInstagram } from "react-icons/ci";
 
 const Speaker = () => {
   return (
-    <div className="bg-[#001530]"> 
+    <div className="bg-[#001530]">
       <div className="h-[100%] w-[100%] bg-[url('https://media.licdn.com/dms/image/C4D12AQGMPq_g1qxXOw/article-cover_image-shrink_720_1280/0/1640755559457?e=1717027200&v=beta&t=aH3ds0Brt5k9Ue6_qRCllyfAGQZSeOQHIMLSEWIYuUg')] bg-no-repeat bg-cover mb-[100px]">
-
-
-<div className='hero-container'>
-  <div className='background'>
-      {/* <img src='https://www.ecell.in/esummit/assets/images/top-left.png' className='top-left'/> */}
-      <img src="	https://www.ecell.in/esummit/assets/images/bottom-left.png" className='bottom-left'/>
-      {/* <img src="https://www.ecell.in/esummit/assets/images/bottom-right.png" className='bottom-right'/> */}
-  </div>
-  <div className='reg-button'>
-      <div className='header'>
-          <span className='green'>SPEAKERS</span>
-          <br/>
-          {/* <span className='white'>EXPO</span> */}
-      </div>
-      
-  </div>
-</div>
+        <div className="hero-container">
+          <div className="background">
+            {/* <img src='https://www.ecell.in/esummit/assets/images/top-left.png' className='top-left'/> */}
+            <img
+              src="	https://www.ecell.in/esummit/assets/images/bottom-left.png"
+              className="bottom-left"
+            />
+            {/* <img src="https://www.ecell.in/esummit/assets/images/bottom-right.png" className='bottom-right'/> */}
+          </div>
+          <div className="reg-button">
+            <div className="header">
+              <span className="green">SPEAKERS</span>
+              <br />
+              {/* <span className='white'>EXPO</span> */}
+            </div>
+          </div>
+        </div>
       </div>
       <div className="flex flex-col justify-center items-center gap-y-10">
         <div className="flex justify-center items-center flex-wrap pl-[10px] pr-[10px]  ">
           <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400 hover:dark:text-gray-300 ml-[5%] mr-[5%] ">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est vitae,
-            cumque reprehenderit molestias quaerat odio culpa. Ratione
-            perferendis, a fugit, eaque doloribus ab maxime totam voluptatem
-            ipsam, animi unde. Iste tempora nobis rem dolores, a ullam
-            consequatur optio nihil dignissimos.
+            Our distinguished speakers bring a wealth of knowledge, experience
+            and innovation to E-Summit 2K24. From business giants to business
+            pioneers, these visionaries will inspire, educate and inspire you to
+            be passionate about business and technology. Join us as they share
+            their insights, stories, and wisdom to guide you on your path to
+            success. Get ready to be inspired, inspired and motivated by our
+            amazing speakers at E-Summit 2K24!
           </p>
         </div>
         <div className="flex flex-col justify-center items-center gap-y-10">
-          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl main-font2 dark:text-white">Our Speakers</h1>
+          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl main-font2 dark:text-white">
+            Our Speakers
+          </h1>
 
           <div className="flex justify-center items-center flex-wrap gap-10 ml-[10%] mr-[10%]">
             <div className="flex justify-center items-center gap-5 flex-wrap mb-[100px]">
@@ -157,63 +161,63 @@ const Speaker = () => {
                 </div>
               </div>
             </div>
+          </div>
+          {/* contact */}
 
-            </div>
-            {/* contact */}
-
-            <div className="flex justify-center items-center flex-wrap flex-col gap-[100px] lg:gap-0">
-              <h1 className="text-white text-2xl lg:text-5xl main-font2">Contact Us</h1>
-              <div className="team_container">
-                {teams.lead.map((team) => (
-                  <div class="wrapper ">
-                    <div class="img-area">
-                      <div class="inner-area">
-                        <img src={team.url} alt="" />
-                      </div>
-                    </div>
-                    <div class="effect"></div>
-                    <div class="name">{team.name}</div>
-                    <div class="secname">{team.name}</div>
-                    <div class="about">{team.designation}</div>
-                    <div class="social-icons">
-                      <a
-                        href="#"
-                        className=" XTwitter flex justify-center items-center text-2xl"
-                      >
-                        <div className="z-40 flex justify-center items-center text-zinc-100 iconbeat ">
-                          <FaXTwitter />
-                        </div>
-                      </a>
-                      <a
-                        href="#"
-                        className="Facebook flex justify-center items-center text-2xl"
-                      >
-                        <div className="z-40 flex justify-center items-center text-zinc-100 iconbeat ">
-                          <FaFacebook />
-                        </div>
-                      </a>
-                      <a
-                        href="#"
-                        className="Linkedin flex justify-center items-center text-2xl"
-                      >
-                        <div className="z-40 flex justify-center items-center text-zinc-100 iconbeat ">
-                          <LiaLinkedinIn />
-                        </div>
-                      </a>
-                      <a
-                        href="#"
-                        className="Instagram flex justify-center items-center text-2xl"
-                      >
-                        <div className="z-40 flex justify-center items-center text-zinc-100 iconbeat ">
-                          <CiInstagram />
-                        </div>
-                      </a>
+          <div className="flex justify-center items-center flex-wrap flex-col gap-[100px] lg:gap-0">
+            <h1 className="text-white text-2xl lg:text-5xl main-font2">
+              Contact Us
+            </h1>
+            <div className="team_container">
+              {teams.lead.map((team) => (
+                <div class="wrapper ">
+                  <div class="img-area">
+                    <div class="inner-area">
+                      <img src={team.url} alt="" />
                     </div>
                   </div>
-                ))}
-              </div>
+                  <div class="effect"></div>
+                  <div class="name">{team.name}</div>
+                  <div class="secname">{team.name}</div>
+                  <div class="about">{team.designation}</div>
+                  <div class="social-icons">
+                    <a
+                      href="#"
+                      className=" XTwitter flex justify-center items-center text-2xl"
+                    >
+                      <div className="z-40 flex justify-center items-center text-zinc-100 iconbeat ">
+                        <FaXTwitter />
+                      </div>
+                    </a>
+                    <a
+                      href="#"
+                      className="Facebook flex justify-center items-center text-2xl"
+                    >
+                      <div className="z-40 flex justify-center items-center text-zinc-100 iconbeat ">
+                        <FaFacebook />
+                      </div>
+                    </a>
+                    <a
+                      href="#"
+                      className="Linkedin flex justify-center items-center text-2xl"
+                    >
+                      <div className="z-40 flex justify-center items-center text-zinc-100 iconbeat ">
+                        <LiaLinkedinIn />
+                      </div>
+                    </a>
+                    <a
+                      href="#"
+                      className="Instagram flex justify-center items-center text-2xl"
+                    >
+                      <div className="z-40 flex justify-center items-center text-zinc-100 iconbeat ">
+                        <CiInstagram />
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              ))}
             </div>
-         
+          </div>
         </div>
       </div>
     </div>
